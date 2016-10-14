700033c58ced3ed3bd3cdf81383eda8fd7a24e2e
Ellen Marie Dash <me@duckie.co>
2016-10-14 07:17:54 -0400

    Make the specs use big endian, too.

--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -18,7 +18,7 @@ describe School do
         0b0001_0001, # Pointer modifier, add instruction.
         0x0,         # OUT register.
         0x4,         # r1 = (2 byte word_size) * 0x2.
-      ].pack('S*')
+      ].pack('S>*')
 
       expect(dsl.bytecode).to eq(expected)
     end
